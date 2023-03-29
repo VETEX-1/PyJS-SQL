0d07a208749511ade874e06e184c705db4afe8f1
vickouma77 <Vickprestone20@gmail.com>
2023-03-30 00:17:44 +0300

    importing CommandHistory module

--- console/console.py
+++ console/console.py
@@ -4,10 +4,11 @@ The console provides an interactive command line interface for users to interact
 with the PyJS-SQL library using Python, SQLAlchemy and MySQLdb.
 
 """
-
-import cmd
 from sqlalchemy import create_engine
 import MySQLdb
+import cmd
+
+from history import CommandHistory
 
 # Create a MySQL database engine
 engine = create_engine('mysql://user:password@localhost/db_name', echo=True)
@@ -23,6 +24,7 @@ class SQLConsole(cmd.Cmd):
 
     intro = 'Welcome to the SQL console. Type help or ? to list commands.\n'
     prompt = '(sql) '
+    history = CommandHistory()
 
     def do_execute(self, arg):
         """Execute an SQL statement."""
@@ -32,6 +34,8 @@ class SQLConsole(cmd.Cmd):
             conn.commit()
             # Print the number of affected rows
             print(f'{cur.rowcount} rows affected.')
+            # Add the command to the history
+            self.history.add_command(arg)
         except Exception as e:
             # Rollback the transaction if an error occurs
             conn.rollback()
@@ -42,6 +46,42 @@ class SQLConsole(cmd.Cmd):
         print('Exiting the SQL console.')
         conn.close()
         return True
+
+    def emptyline(self):
+        """Called when an empty line is entered."""
+        pass
+
+    def do_EOF(self, arg):
+        """Exit the console on EOF."""
+        print('Exiting the SQL console.')
+        conn.close()
+        return True
+
+    def get_command(self):
+        """Override the default method to retrieve the command, allowing for navigation through the command history."""
+        command = input(self.prompt)
+        if command.strip() == '':
+            return ''
+        elif command.strip() == '!!':
+            # Execute the previous command
+            prev_command = self.history.get_previous_command()
+            if prev_command is not None:
+                print(prev_command)
+                return prev_command
+            else:
+                return ''
+        elif command.strip() == '!':
+            # Execute the next command
+            next_command = self.history.get_next_command()
+            if next_command is not None:
+                print(next_command)
+                return next_command
+            else:
+                return ''
+        else:
+            # Add the command to the history
+            self.history.add_command(command)
+            return command
 
 if __name__ == '__main__':
     SQLConsole().cmdloop()
